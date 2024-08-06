1568502cfae4f1648cc346bb2cf0e4dce23d4b5c
Prasanna <prasanna.java.trainer@gmail.com>
2024-08-06 08:48:32 +0530

    code

---
+++ Aug6/EmployProject/src/com/java/dao/EmployDaoImpl.java
@@ -0,0 +1,27 @@
+package com.java.dao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.java.model.Employ;
+
+public class EmployDaoImpl implements EmployDao {
+
+	static List<Employ> employList;
+	
+	static {
+		employList = new ArrayList<Employ>();
+	}
+	
+	@Override
+	public List<Employ> showEmployDao() {
+		return employList;
+	}
+
+	@Override
+	public String addEmployDao(Employ employ) {
+		employList.add(employ);
+		return "Employ Record Inserted...";
+	}
+
+}
